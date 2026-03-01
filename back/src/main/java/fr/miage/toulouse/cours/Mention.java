5b5fffc9ecd3c6fe63b5ab129fdc4a5f825284dd
Marius <marius.cousseins@gmail.com>
2026-03-01 12:21:32 +0100

    Mise a jour classe Mention en fonction du diagramme de classe

--- back/src/main/java/fr/miage/toulouse/cours/Mention.java
+++ back/src/main/java/fr/miage/toulouse/cours/Mention.java
@@ -4,25 +4,28 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Mention {
-
+    private int id;
     private String nom;
+
     private List<Parcour> listParcours = new ArrayList<>();
 
-    public Mention(String nom) {
+    public Mention(int id, String nom) {
         this.nom = nom;
+        this.id = id;
     }
+
+    public String getNom() {
+        return nom;
+    }
+    public void setNom(String nom) { this.nom = nom;}
+
+    public int getId(){return this.id;}
+    public void setId(int id){this.id = id;}
 
     public void addParcour (Parcour parcour){
         this.listParcours.add(parcour);
     }
-    public String getNom() {
-        return nom;
-    }
-
     public List<Parcour> getListParcours() {
         return listParcours;
-    }
-    public void setNom(String nom) {
-        this.nom = nom;
     }
 }
