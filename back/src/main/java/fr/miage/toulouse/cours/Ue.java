4c664d9dbfc857f03777538109fae3233faea6a1
Alex-S06 <alexandre.soubils@toulouse.miage.fr>
2026-03-05 16:51:04 +0100

    Rectification class Ue

--- back/src/main/java/fr/miage/toulouse/cours/Ue.java
+++ back/src/main/java/fr/miage/toulouse/cours/Ue.java
@@ -20,10 +20,13 @@ public class Ue {
     public String getNom() { return nom; }
     public int getNbCredit() { return nbCredit; }
     public int getSemestre() { return semestre; }
+    public Parcour getParcour() {return  this.parcour;}
 
     // --- Setters ---
     public void setCode(String code) { this.code = code; }
     public void setNom(String nom) { this.nom = nom; }
     public void setCredit(int nbCredit) { this.nbCredit = nbCredit; }
     public void setSemestre(int semestre) { this.semestre = semestre; }
+    public void setParcour(Parcour parcour) { this.parcour = parcour; }
+
 }
