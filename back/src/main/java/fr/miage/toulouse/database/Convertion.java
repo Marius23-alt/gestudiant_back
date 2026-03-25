ae6f3fca89486be7858f90ee5af1dc16dde6c2a6
YannKeraudren <yann.keraudren@gmail.com>
2026-03-25 21:20:30 +0100

    Correction et réagencement du package Database

--- back/src/main/java/fr/miage/toulouse/database/Convertion.java
+++ back/src/main/java/fr/miage/toulouse/database/Convertion.java
@@ -1,12 +1,18 @@
 package fr.miage.toulouse.database;
 
 import fr.miage.toulouse.cours.*;
-
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
 
+/**
+ * Classe pour convertir les données de la base de données en objet java
+ */
 public class Convertion {
+
+    private static final String NOM_MENTION="nom_mention";
+    private static final String NOM_PARCOURS="nom_parcours";
+    private static final String ID_MENTION="id_mention";
+    private static final String ID_PARCOURS="id_parcours";
 
     private Convertion () {}
 
@@ -17,13 +23,13 @@ public class Convertion {
 
         // On fabrique d'abord la Mention (la plus petite poupée russe)
         Mention mention = new Mention(
-                rs.getInt("id_mention"),
+                rs.getInt(ID_MENTION),
                 rs.getString("mention")
         );
 
         // On fabrique ensuite le Parcours (et on met la Mention dedans)
         Parcour parcour = new Parcour(
-                rs.getInt("id_parcours"),
+                rs.getInt(ID_PARCOURS),
                 rs.getString("parcour"),
                 mention
         );
@@ -40,31 +46,51 @@ public class Convertion {
         );
     }
 
-    // On laisse ces méthodes en bas au cas où, mais c'est surtout toEtudiant qui fera tout le travail
+    /**
+     * Convertit une ligne de la table Parcours en un objet java parcour mais rattaché à aucun parcours
+     * @param rs le résultat de la requête de récupération d'un parcours
+     * @return l'objet parcour crée
+     * @throws SQLException renvoie une exception SQL s'il y a eu une erreur lors
+     * de la récupération du tuple
+     */
     public static Parcour toParcour(ResultSet rs) throws SQLException {
-        return new Parcour(rs.getInt("id_parcours"), rs.getString("nom_parcours"), null);
+        return new Parcour(rs.getInt(ID_PARCOURS), rs.getString(NOM_PARCOURS), null);
     }
 
+    /**
+     * Convertit une ligne de la table Mention en un objet java mention
+     * @param rs le résultat de la requête de récupération de la mention
+     * @return l'objet mention crée
+     * @throws SQLException renvoie une exception SQL s'il y a eu une erreur lors
+     * de la récupération du tuple
+     */
     public static Mention toMention(ResultSet rs) throws SQLException {
-        return new Mention(rs.getInt("id_mention"), rs.getString("nom_mention"));
+        return new Mention(rs.getInt(ID_MENTION), rs.getString(NOM_MENTION));
     }
 
+    /**
+     * Convertit une ligne de la table Ue en un objet java parcour mais rattaché à aucun parcours ni mention
+     * @param rs le résultat de la requête de récupération d'une ue
+     * @return l'objet ue crée
+     * @throws SQLException renvoie une exception SQL s'il y a eu une erreur lors
+     * de la récupération du tuple
+     */
     public static Ue toUe(ResultSet rs) throws SQLException {
         Mention mention = null;
         Parcour parcour = null;
 
 
-        if (rs.getString("nom_mention") != null) {
+        if (rs.getString("NOM_MENTION") != null) {
             mention = new Mention(
-                    rs.getInt("id_mention"),
-                    rs.getString("nom_mention")
+                    rs.getInt(ID_MENTION),
+                    rs.getString("NOM_MENTION")
             );
         }
 
-        if (rs.getString("nom_parcours") != null) {
+        if (rs.getString(NOM_PARCOURS) != null) {
             parcour = new Parcour(
-                    rs.getInt("id_parcours"),
-                    rs.getString("nom_parcours"),
+                    rs.getInt(ID_PARCOURS),
+                    rs.getString(NOM_PARCOURS),
                     mention
             );
         }
